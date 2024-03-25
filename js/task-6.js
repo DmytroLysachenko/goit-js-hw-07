59f4bbbfe4edb0eb1d5224da8b2399a5f51fb0ba
Dimka863 <dlysachenko98@gmail.com>
2024-03-25 09:40:51 +0100

    Done HW

--- js/task-6.js
+++ js/task-6.js
@@ -3,24 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
 const divBoxes = document.querySelector("div#boxes");
 const input = document.querySelector("input");
-const createButton = document.querySelector('button[data="create"]');
-const destroyButton = document.querySelector('button[data="destroy"]');
-const inputValue = input.value;
-function createBoxes(amount) {
-  for (let index = 0; index < amount; index++) {
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+function createBoxes(event) {
+  divBoxes.innerHTML = "";
+  if (input.value < 1 || input.value > 100) {
+    input.value = "";
+    return alert("Please type number from 1 to 100");
+  }
+  for (let index = 0; index < input.value; index++) {
     divBoxes.insertAdjacentHTML("beforeend", "<div></div>");
     divBoxes.lastChild.style.backgroundColor = getRandomHexColor();
     divBoxes.lastChild.style.width = `${30 + index * 10}px`;
     divBoxes.lastChild.style.height = `${30 + index * 10}px`;
   }
-  input.reset();
+  input.value = "";
 }
 
-console.log(inputValue);
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
 
-function destroyBoxes() {}
-
-createButton.addEventListener("click", createBoxes(inputValue));
+createButton.addEventListener("click", createBoxes);
 destroyButton.addEventListener("click", destroyBoxes);
