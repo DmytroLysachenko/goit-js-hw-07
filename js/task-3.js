06a3e71429ad43cb293cbf8ae0e6034ca5313ca2
Dimka863 <dlysachenko98@gmail.com>
2024-03-26 16:02:19 +0100

    Upd with ""check

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("input#name-input");
 const output = document.querySelector("span#name-output");
-input.addEventListener(
-  "input",
-  (event) => (output.textContent = event.currentTarget.value.trim())
+input.addEventListener("input", (event) =>
+  input.value.trim() === ""
+    ? (output.textContent = "Anonymous")
+    : (output.textContent = event.currentTarget.value.trim())
 );
